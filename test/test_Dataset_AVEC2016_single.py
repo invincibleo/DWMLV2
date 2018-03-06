9978e605f01e38d92cf0b2fa4e304cc039083263
Duowei Tang <tangduowei@live.com>
2018-03-06 15:24:37 +0100

    print and .tolist() in test_file

--- test/test_Dataset_AVEC2016_single.py
+++ test/test_Dataset_AVEC2016_single.py
@@ -5,13 +5,8 @@ from hyperopt import fmin, tpe, hp
 import os, sys
 sys.path.append(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
 
-<<<<<<< HEAD
-from application.Dataset_AVEC2016 import *
-from application.LearnerLSTMReg import *
-=======
 from application.Dataset_AVEC2016_V2 import *
 from application.LearnerLSTMReg_V2 import *
->>>>>>> 9f37874ba27b16d5192ad783a02017d4b6e101b3
 from application.Evaluator_AVEC2016 import *
 from application.LearnerInceptionV3 import LearnerInceptionV3
 from core.evaluation import DCASE2016_EventDetection_SegmentBasedMetrics
@@ -131,7 +126,6 @@ class MyTestCase(unittest.TestCase):
         )
         FLAGS, unparsed = parser.parse_known_args()
 
-        setup_keras()
         dataset = Dataset_AVEC2016(dataset_dir=DATASET_DIR, flag=FLAGS, normalization=False, dimension=FLAGS.dimension, using_existing_features=False)
         learner = LearnerLSTMReg(dataset=dataset, learner_name='LSTMReg', flag=FLAGS)
         evaluator = Evaluator_AVEC2016()
@@ -140,6 +134,7 @@ class MyTestCase(unittest.TestCase):
         truth, prediction = learner.predict()
         evaluator.evaluate(truth, prediction)
         results = evaluator.results()
+        print(results)
 
         results_dir_addr = 'tmp/results/'
         current_time_str = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
@@ -150,8 +145,8 @@ class MyTestCase(unittest.TestCase):
         if not tf.gfile.Exists(results_file_dir):
             tf.gfile.MakeDirs(results_file_dir)
             json.dump(results, open(results_file_dir + '/results_' + current_time_str + '.json', 'wb'), indent=4)
-            json.dump(zip(truth[:, 0], prediction[:, 0].tolist()), open(results_file_dir + '/results_' + current_time_str + '_0.json', 'a'), indent=4)
-            json.dump(zip(truth[:, 1], prediction[:, 1].tolist()), open(results_file_dir + '/results_' + current_time_str + '_1.json', 'a'), indent=4)
+            json.dump(zip(truth[:, 0].tolist(), prediction[:, 0].tolist()), open(results_file_dir + '/results_' + current_time_str + '_0.json', 'a'), indent=4)
+            json.dump(zip(truth[:, 1].tolist(), prediction[:, 1].tolist()), open(results_file_dir + '/results_' + current_time_str + '_1.json', 'a'), indent=4)
             with open(results_file_dir + 'FLAGS_' + current_time_str + '.txt', 'wb') as f:
                 f.write(str(FLAGS))
 
