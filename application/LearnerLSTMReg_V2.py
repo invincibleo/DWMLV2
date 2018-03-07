5fb5ecc7e33333a4913763b5edae529e97c4477c
invincibleo <tangduowei@live.com>
2018-03-07 16:02:42 +0100

    stateful LSTM batch size 1

--- application/LearnerLSTMReg_V2.py
+++ application/LearnerLSTMReg_V2.py
@@ -56,22 +56,11 @@ class LearnerLSTMReg(Learner):
         if not os.path.exists(model_json_file_addr) or continue_training:
             self.copy_configuration_code()  # copy the configuration code so that known in which condition the model is trained
 
-            # expected input data shape: (batch_size, timesteps, data_dim)
-
-
-            # input_l = Input(shape=(num_t_x, 1))
-            # # x = LSTM(num_t_x, stateful=False)(input_l)
-            # x = keras.layers.LSTM(5, return_sequences=True)(input_l)
-            # x = keras.layers.LSTM(5, return_sequences=True)(x)
-            # x = keras.layers.LSTM(5)(x)
-            # x = Dense(5, activation='relu')(x)
-            # out = Dense(1, activation='linear')(x)
-            # model = Model(input_l, out, "LSTM_reg_v2")
-
-            # model = LSTM_MIMO(num_t_x=num_t_x, num_input_dims=88, num_states=64, batch_size=self.FLAGS.train_batch_size)
-
-            model = SoundNet()
-            model.add(LSTM(64, dropout=0.5))    # dropout set as the AVEC 2017 paper
+            model = Sequential()
+            model.add(LSTM(128, batch_input_shape=(self.FLAGS.train_batch_size, 1, 1024), return_sequences=False, stateful=True))
+            model.add(Dropout(0.5))# dropout set as the AVEC 2017 paper
+            # model.add(LSTM(256, batch_input_shape=(self.FLAGS.train_batch_size, 1, 512), return_sequences=False, stateful=True))
+            # model.add(Dropout(0.5))# dropout set as the AVEC 2017 paper
             model.add(Dense(2, activation='tanh'))
 
             if continue_training:
@@ -81,7 +70,7 @@ class LearnerLSTMReg(Learner):
             model.compile(loss='mean_squared_error',
                           optimizer=keras.optimizers.Adam(lr=self.FLAGS.learning_rate,
                                                           beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0),
-                          metrics=[CCC, 'mse'])
+                          metrics=[CCC, 'mae'])
 
             # callbacks
             if tf.gfile.Exists('tmp/logs/tensorboard/' + str(self.hash_name_hashed)):
@@ -98,16 +87,7 @@ class LearnerLSTMReg(Learner):
                                                                      factor=0.1,
                                                                      patience=10,
                                                                      epsilon=0.0005)
-            # def schedule(epoch_num):
-            #     if epoch_num <= 30:
-            #         learning_rate = 0.01
-            #     elif epoch_num > 30 and epoch_num <= 60:
-            #         learning_rate = 0.001
-            #     elif epoch_num > 60 and epoch_num <= 90:
-            #         learning_rate = 0.0005
-            #     else:
-            #         learning_rate = 0.0001
-            #     return learning_rate
+
             def schedule(epoch):
                 initial_lrate = 0.01
                 drop = 0.5
@@ -117,40 +97,18 @@ class LearnerLSTMReg(Learner):
 
             learning_rate_schedule = keras.callbacks.LearningRateScheduler(schedule=schedule)
 
-            # training_generator = self.dataset.generate_sequencial_batch_data(category='training', num_t_x=num_t_x,
-            #                                                                  overlap=0.0,
-            #                                                                  batch_size=self.FLAGS.train_batch_size,
-            #                                                                  input_shape=self.input_shape)
-            # validation_generator = self.dataset.generate_sequencial_batch_data(category='validation', num_t_x=num_t_x,
-            #                                                                    overlap=0.0,
-            #                                                                    batch_size=self.FLAGS.validation_batch_size,
-            #                                                                    input_shape=self.input_shape)
+
             model.summary()
-            # hist = model.fit_generator(
-            #     generator=training_generator,
-            #     steps_per_epoch=int(self.dataset.num_training_data/self.FLAGS.train_batch_size/num_t_x),
-            #     # initial_epoch=100,
-            #     epochs=15,
-            #     callbacks=[tensorboard, learning_rate_schedule],
-            #     validation_data=validation_generator,
-            #     validation_steps=int(self.dataset.num_validation_data/self.FLAGS.train_batch_size/num_t_x),
-            #     max_q_size=30,
-            #     workers=20
-            # )
-            # hist = model.fit(aa, bb,
-            #                  batch_size=self.FLAGS.train_batch_size,
-            #                  epochs=1000000,
-            #                  verbose=1,
-            #                  callbacks=[tensorboard, learning_rate_schedule],
-            #                  validation_data=(a11, b11),
-            #                  shuffle=True)
-            hist = model.fit(self.dataset.training_total_features, self.dataset.training_total_labels,
-                             batch_size=self.FLAGS.train_batch_size,
-                             epochs=200,
-                             verbose=1,
-                             callbacks=[tensorboard, learning_rate_schedule],
-                             validation_data=(self.dataset.validation_total_features, self.dataset.validation_total_labels),
-                             shuffle=True)
+
+            for i in range(200):
+                hist = model.fit(self.dataset.training_total_features, self.dataset.training_total_labels,
+                                 batch_size=self.FLAGS.train_batch_size,
+                                 epochs=1,
+                                 verbose=1,
+                                 callbacks=[tensorboard, learning_rate_schedule],
+                                 validation_data=(self.dataset.validation_total_features, self.dataset.validation_total_labels),
+                                 shuffle=True)
+                model.reset_states()
 
             # save the model and training history
             self.save_model(hist, model)
@@ -169,22 +127,7 @@ class LearnerLSTMReg(Learner):
         # load weights into new model
         model.load_weights(model_h5_file_addr)
 
-        # generator = self.dataset.generate_sequencial_batch_data(category='validation',
-        #                                                         num_t_x=num_t_x,
-        #                                                         overlap=0.0,
-        #                                                         batch_size=self.FLAGS.test_batch_size,
-        #                                                         input_shape=self.input_shape)
-        # Y_all = []
-        # predictions_all = []
-        # for i in range(5): # range(int(self.dataset.num_testing_data/num_t_x/self.FLAGS.test_batch_size)):
-        #     X, Y = generator.next()
-        #     predictions = model.predict_on_batch(X)
-        #     Y_all.append(Y)
-        #     predictions_all.append(predictions)
         predictions_all = model.predict(self.dataset.validation_total_features, batch_size=256, verbose=0)
-        # predictions_all = model.predict(a11, batch_size=256, verbose=0)
-        # predictions_all_all = predictions_all * b_std + b_mean
-        # plt.plot(predictions_all_all)
 
         Y_all = self.dataset.validation_total_labels
 
