d02411e0df5b50d47d66ffa57b47661ff7638dfa
invincibleo <tangduowei@live.com>
2018-03-27 16:10:15 +0200

    try using classification in regression

--- application/LearnerLSTMReg_V2.py
+++ application/LearnerLSTMReg_V2.py
@@ -34,9 +34,40 @@ class LearnerLSTMReg(Learner):
             self.copy_configuration_code()  # copy the configuration code so that known in which condition the model is trained
 
             # expected input data shape: (batch_size, timesteps, data_dim)
+            labels = self.dataset.training_total_labels
+            class_num = 1000
+            labels = np.matrix.round((labels * class_num + 1000).astype('int32'))
+            one_hot_labels = np.zeros((labels.shape + (class_num,)))
+            for i in range(labels.shape[0]):
+                one_hot = np.zeros((2, class_num))
+                arousal_idx = int(labels[i, 0] / 2)
+                valence_idx = int(labels[i, 1] / 2)
+                one_hot[0, arousal_idx] = 1
+                one_hot[1, valence_idx] = 1
+                one_hot_labels[i, :, :] = one_hot
+            one_hot_labels = np.reshape(one_hot_labels, (-1, 2 * class_num))
+            self.dataset.training_total_labels = one_hot_labels
+
+            labels = self.dataset.validation_total_labels
+            class_num = 1000
+            labels = np.matrix.round((labels + 1) * class_num).astype('int32')
+            one_hot_labels = np.zeros((labels.shape + (class_num,)))
+            for i in range(labels.shape[0]):
+                one_hot = np.zeros((2, class_num))
+                arousal_idx = int(labels[i, 0] / 2)
+                valence_idx = int(labels[i, 1] / 2)
+                one_hot[0, arousal_idx] = 1
+                one_hot[1, valence_idx] = 1
+                one_hot_labels[i, :, :] = one_hot
+            one_hot_labels = np.reshape(one_hot_labels, (-1, 2 * class_num))
+            self.dataset.validation_total_labels = one_hot_labels
 
             model = SoundNet()
             model.add(Flatten())
+            model.add(Dense(4096))
+            model.add(BatchNormalization())
+            model.add(Activation('relu'))
+            model.add(Dropout(0.2))
             # model.add(LSTM(512, batch_input_shape=(self.FLAGS.train_batch_size, 1, 401), stateful=True,
             #                dropout=0.1))
             # model.add(Dense(512))
@@ -47,16 +78,15 @@ class LearnerLSTMReg(Learner):
             # model.add(BatchNormalization())
             # model.add(Activation('relu'))
             # model.add(Dropout(0.1))
-            model.add(Dense(2, activation='linear', activity_regularizer=keras.regularizers.l2(0.0001)))
+            model.add(Dense(2*class_num, activation='softmax', activity_regularizer=keras.regularizers.l2(0.0001)))
 
             if continue_training:
                 model.load_weights("tmp/model/" + self.hash_name_hashed + "/model.h5")  # load weights into new model
 
             # Compile model
-            model.compile(loss='mean_squared_error',
-                          optimizer=keras.optimizers.Adam(lr=self.FLAGS.learning_rate,
-                                                          beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0),
-                          metrics=[CCC, 'mae'])
+            model.compile(loss='categorical_crossentropy',
+                          optimizer=keras.optimizers.Adam(lr=self.FLAGS.learning_rate),
+                          metrics=['categorical_accuracy'])
 
             # callbacks
             if tf.gfile.Exists('tmp/logs/tensorboard/' + str(self.hash_name_hashed)):
@@ -74,9 +104,9 @@ class LearnerLSTMReg(Learner):
                                                                      patience=10,
                                                                      epsilon=0.0005)
             def schedule(epoch):
-                if epoch < 30:
+                if epoch < 25:
                     return 0.01
-                elif 30 <= epoch <= 200:
+                elif 25 <= epoch <= 50:
                     return 0.001
                 else:
                     return 0.0001
@@ -86,21 +116,13 @@ class LearnerLSTMReg(Learner):
             learning_rate_schedule = keras.callbacks.LearningRateScheduler(schedule=schedule)
 
             model.summary()
-            for i in range(500):
-                lr = schedule(i)
-                model.compile(loss='mean_squared_error',
-                              optimizer=keras.optimizers.Adam(lr=lr,
-                                                              beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0),
-                              metrics=[CCC, 'mae'])
-                hist = model.fit(self.dataset.training_total_features, self.dataset.training_total_labels,
-                                 batch_size=self.FLAGS.train_batch_size,
-                                 initial_epoch=i,
-                                 epochs=i+1,
-                                 verbose=1,
-                                 callbacks=[tensorboard],
-                                 validation_data=(self.dataset.validation_total_features, self.dataset.validation_total_labels),
-                                 shuffle=False)
-                model.reset_states()
+            hist = model.fit(self.dataset.training_total_features, self.dataset.training_total_labels,
+                             batch_size=self.FLAGS.train_batch_size,
+                             epochs=100,
+                             verbose=1,
+                             callbacks=[tensorboard, learning_rate_schedule],
+                             validation_data=(self.dataset.validation_total_features, self.dataset.validation_total_labels),
+                             shuffle=True)
 
             # save the model and training history
             self.save_model(hist, model)
@@ -120,6 +142,10 @@ class LearnerLSTMReg(Learner):
         model.load_weights(model_h5_file_addr)
 
         predictions_all = model.predict(self.dataset.validation_total_features, batch_size=self.FLAGS.validation_batch_size, verbose=0)
+
+        predictions_all = np.reshape(predictions_all, (predictions_all.shape[0], 2, -1))
+        predictions_all = np.argmax(predictions_all, axis=2)
+        predictions_all = (predictions_all * 2 + 1000) / 1000
 
         Y_all = self.dataset.validation_total_labels
 
